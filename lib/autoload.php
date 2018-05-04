92a1a32c18e270420580ad8fe44b7e5481ef6301
Bence Skorka <skorka.bence@gmail.com>
2018-05-04 16:12:00 +0200

    preparations

---
+++ lib/autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+define("ROOT_DIR", __DIR__ . "/..");
+
+require_once(__DIR__ . "/vendor/autoload.php");
+
+require_once(__DIR__ . "/BookShelf/AutoLoader.php");
+BookShelf\AutoLoader::register();
