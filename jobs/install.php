47311f9ad5b946cd5a0c8afecb910c278d714ca7
Bence Skorka <skorka.bence@gmail.com>
2018-05-15 22:38:31 +0200

    installation

---
+++ jobs/install.php
@@ -0,0 +1,61 @@
+<?php
+echo "KONYVESPOLC INSTALLER WIZARD" . PHP_EOL;
+echo file_get_contents(__DIR__ . "/wizard.txt") . PHP_EOL;
+
+// Config -----------------------------------------------------------------
+
+if (!file_exists(__DIR__ . "/../config.php")) {
+    echo "The config file is missing." . PHP_EOL;
+    echo "MySQL server:" . PHP_EOL;
+    echo "Host: ";
+    $host = readline();
+    echo "Database: ";
+    $db = readline();
+    echo "Username: ";
+    $user = readline();
+    echo "Password: ";
+    $pw = readline();
+    echo "reCAPTCHA:" . PHP_EOL;
+    echo "Size key: ";
+    $sik = readline();
+    echo "Secret key: ";
+    $sek = readline();
+
+    file_put_contents(__DIR__ . "/../config.json", json_encode([
+        "mysql" => [
+            "host" => $host,
+            "username" => $user,
+            "password" => $pw,
+            "database" => $db
+        ],
+        "recaptcha" => [
+            "site_key" => $sik,
+            "secret" => $sek
+        ]
+    ]));
+    file_put_contents(__DIR__ . "/../config.php", '<?php $_CONFIG=json_decode(file_get_contents(__DIR__ . "/config.json"), true);');
+} else {
+    echo "Config file exists." . PHP_EOL;
+}
+
+// SQL  -----------------------------------------------------------------
+
+echo "Running SQL script..." . PHP_EOL;
+shell_exec("cd " . escapeshellarg(__DIR__ . "/") . " && php init_db.php");
+
+// Templates   -----------------------------------------------------------------
+
+echo "Running template script..." . PHP_EOL;
+shell_exec("cd " . escapeshellarg(__DIR__ . "/") . " && php init_templates.php");
+
+// TypeScript   -----------------------------------------------------------------
+
+echo "Compiling typescirpt..." . PHP_EOL;
+shell_exec("cd " . escapeshellarg(__DIR__ . "/../") . " && tsc -p tsconfig.json");
+
+// Composer   -----------------------------------------------------------------
+
+echo "Updating composer..." . PHP_EOL;
+shell_exec("cd " . escapeshellarg(__DIR__ . "/../lib/") . " && composer update");
+
+echo "Done." . PHP_EOL;
